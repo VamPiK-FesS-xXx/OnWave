2c1135afd8ca14c4c5e8e62be185696eef16e5f3
Vampq <vampik077@gmail.com>
2025-09-09 22:58:33 +0300

    access sc token

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,44 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useClickOutside } from '../../scripts/useClickOutside'
 import RenderNavLinks from './navLinksTemplate'
 import RenderDropDownLinks from './navDropdownLinks'
 import './styles/header.css'
+import logo from '../../../public/logo/logo.svg'
+const CLIENT_ID = 'gITscv1hq8CWdM22JLhtJHp6qmygRiRv'
+const CLIENT_SECRET = 'Dr8MOQh42CMKtL3xgUM8uORnkaIXxFTJ'
 export default function Header() {
+	const [accessToken, setAccessToken] = useState('')
 	const [isOpen, setOpen] = useState(false)
 	const menuRef = useRef(null)
 	useClickOutside(menuRef, () => {
 		if (isOpen) setTimeout(() => setOpen(false), 50)
 	})
+	useEffect(() => {
+		let authParam = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Accept: 'application/json; charset=utf-8',
+				Authorization: 'CLIENT_ID; CLIENT_SECRET',
+			},
 
+			body:
+				'grant_type=client_credentials&client_id=' +
+				CLIENT_ID +
+				'&client_secret=' +
+				CLIENT_SECRET,
+		}
+		fetch('https://api.soundcloud.com/oauth2/token', authParam)
+			.then(result => result.json())
+			.then(data => console.log(data))
+	}, [])
 	return (
 		<>
 			<header className='header'>
 				<div className='header-container'>
 					<div className='container-logo'>
 						<a href='/' className='logo-link'>
-							<img
-								src='/public/logo/logo.svg'
-								alt='OnWave-logo'
-								className='logo-img'
-							/>
+							<img src={logo} alt='OnWave-logo' className='logo-img' />
 						</a>
 					</div>
 					<nav className='list-container'>
